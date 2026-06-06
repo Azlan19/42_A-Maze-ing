cc4eeb3e89a8d0d4b50eb0276caa0cec7867b842
Azlan19 <omarazlan6@gmail.com>
2026-06-06 16:09:06 +0200

    Start main

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -32,3 +32,35 @@ def _generate(
     # Helper function because need to generate the maze twice: at startup and again every rerun
 
 
+def main() -> None:
+    if len(sys.argv) != 2:
+        print("Usage: python3 a_maze_ing.py <config_file>")
+    sys.exit(1)
+
+    cfg = parse_config(sys.argv[1])
+    
+    if cfg.seed is not None:
+        seed = cfg.seed
+    else:
+        seed = 0
+    
+    mg, path = _generate(cfg, seed)
+    wall_colour_index = 0
+    show_path = True
+
+    while True:
+        _clear()
+        render(
+            mg.grid, mg.width, mg.height,
+            mg.entry, mg.exit,
+            path, show_path,
+            WALL_COLOURS[wall_colour_index],
+            mg.pattern_cells
+        )
+
+        print("\n=== A-Maze-ing ===")
+        print("\n1. Re-generate a new maze")
+        print("2. Show/Hide path from entry to exit")
+        print("3. Rotate maze colours")
+        print("4. Quit")
+        choice = input("Choice? (1-4): ").strip()
