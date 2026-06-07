f2fe92d6c4d9e3018001e677725d859d063bdc8b
Omar Azlan <oazlan@3-H-7.42heilbronn.de>
2026-06-07 16:58:21 +0200

    flake8 and mypy --strict

--- mazegen/__init__.py
+++ mazegen/__init__.py
@@ -7,7 +7,8 @@ __all__ = ["MazeGenerator", "solve"]
 # === Used to show how it works in a venv ===
 # from mazegen import MazeGenerator, solve
 
-# mg = MazeGenerator(width=11, height=9, seed=42, perfect=True, entry=(0, 0), exit=(10, 8))
+# mg = MazeGenerator(width=11, height=9, seed=42,
+# perfect=True, entry=(0, 0), exit=(10, 8))
 # mg.generate()
 # path = solve(mg.grid, mg.entry, mg.exit, mg.width, mg.height)
 
